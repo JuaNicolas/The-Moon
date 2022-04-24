880ad9be7037c0d067ed06fb329f0832bdd41681
Nicolas Juanico <nicolas.juany@gmail.com>
2022-04-24 12:56:38 +0200

    Lint

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-
 navSlide = () => {
-    let moon = document.querySelector('.moon')
-    let nav = document.querySelector('.nav-links')
-    // Toggle nav
-    moon.addEventListener('click', () => {
-        nav.classList.toggle('nav-active')
-    })
+  let moon = document.querySelector(".moon");
+  let nav = document.querySelector(".nav-links");
+  // Toggle nav
+  moon.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
 
-}
+window.addEventListener("scroll", function () {
+  let moon = document.querySelector(".moon");
+  let nav = document.querySelector(".nav-links");
 
-window.addEventListener('scroll', function () {
-    let moon = document.querySelector('.moon')
-    let nav = document.querySelector('.nav-links')
-
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 80) {
-
-        moon.style.position = 'fixed'
-        moon.style.top = '5vh'
-        moon.style.right = '5vw'
-        nav.style.position = 'fixed'
-    } else {
-        moon.style.position = 'initial'
-    }
-})
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 80
+  ) {
+    moon.style.position = "fixed";
+    moon.style.top = "5vh";
+    moon.style.right = "5vw";
+    nav.style.position = "fixed";
+  } else {
+    moon.style.position = "initial";
+  }
+});
 
 window.onload = function () {
-    navSlide()
-}
+  navSlide();
+};
